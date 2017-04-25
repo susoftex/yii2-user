5a8b1fdde9cd295128d3a5c62d5911c5b7f18b1f
Yuriy Basov <basowy@gmail.com>
2017-04-25 12:17:13 -0700

    user model and table migration

---
+++ migrations/m170425_191046_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `user`.
+ */
+class m170425_191046_create_user_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(20),
+            'username' => $this->string(200)->defaultValue(NULL)->unique(),
+            'password_hash' => $this->string(255)->defaultValue(NULL),
+            'auth_key' => $this->string(255)->defaultValue(NULL),
+            'created_at' => $this->datetime()->defaultValue(NULL),
+            'created_by' => $this->integer(20)->defaultValue(NULL),
+            'updated_at' => $this->datetime()->defaultValue(NULL),
+            'updated_by' => $this->integer(20)->defaultValue(NULL),
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+}
---
+++ models/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace yiiext\user\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password_hash
+ * @property string $auth_key
+ * @property string $created_at
+ * @property int $created_by
+ * @property string $updated_at
+ * @property int $updated_by
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['created_at', 'updated_at'], 'safe'],
+            [['created_by', 'updated_by'], 'integer'],
+            [['username'], 'string', 'max' => 200],
+            [['password_hash', 'auth_key'], 'string', 'max' => 255],
+            [['username'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password_hash' => 'Password Hash',
+            'auth_key' => 'Auth Key',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+            'updated_at' => 'Updated At',
+            'updated_by' => 'Updated By',
+        ];
+    }
+}
